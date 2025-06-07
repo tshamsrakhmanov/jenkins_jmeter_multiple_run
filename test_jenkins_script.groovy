e82689c442f5bc0866a7fe209a8f58032a4cecae
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-06-07 19:45:11 +0000

    add jenkins script

---
+++ test_jenkins_script.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent {
+        label 'server2'  // Targets a specific agent
+        // or use 'any' to run on any available agent
+    }
+    
+    environment {
+        TEST_VALUE_1 = '1'
+        TEST_VALUE_2 = '2'
+    }
+    
+    stages {
+        stage('step1 - copy git') {
+            steps {
+                git url: 'https://github.com/your/repo.git'
+            }
+        }
+
+    }
+	
+	stages {
+        stage('step2 - run the script') {
+            steps {
+                sh test_script.sh
+            }
+        }
+
+    }
+}
