35ac06dcd8cf421ed90c2532b73504fff94dfd3e
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-06-07 22:43:06 +0000

    stub host - add killer of previous stubs in memory

--- script_stub_host.sh
+++ script_stub_host.sh
@@ -5,3 +5,6 @@ host_name=$(hostname)
 host_ip=$(hostname -I)
 echo "SERVER : ${host_name}"
 echo "IP : ${host_ip}"
+
+# kill any other stubs processes:
+kill -9 $(ps aux | grep 'stub-1.jar' | grep -v grep | awk '{print $2}')
--- stub/src/main/java/org/example/RestController.java
+++ stub/src/main/java/org/example/RestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RestController {
     @GetMapping(value = "/get_sample")
     public String get_uuid_list() {
+        System.out.println("returned answer");
         return "ANSWER";
     }
 }
