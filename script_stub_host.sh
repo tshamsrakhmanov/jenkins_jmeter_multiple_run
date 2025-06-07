5f8c38b9358687c5c44f1483d39a5f6a8fbf4b32
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-06-07 22:45:14 +0000

    check dir in stub host

--- script_stub_host.sh
+++ script_stub_host.sh
@@ -8,3 +8,6 @@ echo "IP : ${host_ip}"
 
 # kill any other stubs processes:
 kill -9 $(ps aux | grep 'stub-1.jar' | grep -v grep | awk '{print $2}')
+
+current_dir=$(pwd)
+echo "Current dir: ${current_dir}"
