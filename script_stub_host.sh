f26ab0f1219fd1a6d32696d39d1422636e8c7309
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-06-08 09:34:39 +0000

    add docker cleanup in stub script

--- script_stub_host.sh
+++ script_stub_host.sh
@@ -1,13 +1,34 @@
 #! /bin/bash
 
+
+#
+# welcome message
+#
 echo " ---> SCRIPT FOR STUB HOST"
 host_name=$(hostname)
 host_ip=$(hostname -I)
 echo "SERVER : ${host_name}"
 echo "IP : ${host_ip}"
 
+#
 # kill any other stubs processes:
+#
 kill -9 $(ps aux | grep 'stub-1.jar' | grep -v grep | awk '{print $2}')
+
+#
+# stop all docker containers
+#
+sudo docker stop $(sudo docker ps -a -q) && sudo docker rm $(sudo docker ps -a -q)
+
+#
+# remove all previous docker images
+#
+docker rmi -f $(docker images -aq)
+
+
+#
+# build stub JAR
+#
 
 current_dir=$(pwd)
 echo "Current dir: ${current_dir}"
