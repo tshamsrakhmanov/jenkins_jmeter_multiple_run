4c2b39b80bd9c776813b2977649be1a830922f01
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-06-08 09:51:17 +0000

    docker debug

--- script_stub_host.sh
+++ script_stub_host.sh
@@ -4,35 +4,46 @@
 #
 # welcome message
 #
+echo " #########################"
 echo " ---> SCRIPT FOR STUB HOST"
+echo " #########################"
 host_name=$(hostname)
 host_ip=$(hostname -I)
 echo "SERVER : ${host_name}"
 echo "IP : ${host_ip}"
+echo "$(whoami)"
 
 #
 # kill any other stubs processes:
 #
+echo " #########################"
 echo " ---> TRY TO KILL STUB PROCESSES"
+echo " #########################"
 kill -9 $(ps aux | grep 'stub-1.jar' | grep -v grep | awk '{print $2}')
 
 #
 # stop all docker containers
 #
+echo " #########################"
 echo " ---> STOP DOCKER CONTAINERS"
+echo " #########################"
 sudo -S docker stop $(sudo docker ps -a -q) && sudo -S docker rm $(sudo docker ps -a -q)
 
 #
 # remove all previous docker images
 #
+echo " #########################"
 echo " ---> TRY TO REMOVE ALL DOCKER IMAGES"
+echo " #########################"
 docker rmi -f $(docker images -aq)
 
 
 #
 # build stub JAR
 #
+echo " #########################"
 echo " ---> BUILD STUB JAR"
+echo " #########################"
 current_dir=$(pwd)
 echo "Current dir: ${current_dir}"
 cd stub/
